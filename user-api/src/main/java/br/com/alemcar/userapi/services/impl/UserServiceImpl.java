d32b9d8b5b4443402fcff5f8bad5ca47ef7727ea
Alessandro <sandromuniz27@hotmail.com>
2022-05-12 15:59:45 -0400

    feat: eureka server created

--- user-api/src/main/java/br/com/alemcar/userapi/services/impl/UserServiceImpl.java
+++ user-api/src/main/java/br/com/alemcar/userapi/services/impl/UserServiceImpl.java
@@ -5,19 +5,23 @@ import br.com.alemcar.userapi.repositories.UserRepository;
 import br.com.alemcar.userapi.services.UserService;
 import br.com.alemcar.userapi.services.exceptions.ObjectNotFoundException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class UserServiceImpl implements UserService {
 
     private final UserRepository repository;
+    private final Environment env;
 
     @Override
     public User findById(Long id) {
+        log.info("USER_SERVICE ::: Get request on " + env.getProperty("local.server.port") + " port");
         return repository.findById(id).orElseThrow(() -> new ObjectNotFoundException("Object not found"));
     }
 
